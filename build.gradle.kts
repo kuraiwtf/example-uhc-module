4f71687c145ed02147d4d33fa9619fef44797ff9
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-09-04 18:23:55 +0200

    chore: update UHC API to v1.5.0 and add GitHubPackages Maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,16 @@ repositories {
   mavenCentral()
   mavenLocal()
 
+  maven {
+    name = "GitHubPackages"
+    url = uri("https://maven.pkg.github.com/kuraiwtf/uhc")
+    credentials {
+      username = (project.findProperty("githubActor") ?: System.getenv("USERNAME")) as String?
+      password = (project.findProperty("githubPassword") ?: System.getenv("TOKEN")) as String?
+    }
+  }
+
+  maven("https://jitpack.io")
   maven("https://repo.lunarclient.dev")
   maven("https://repo.j4c0b3y.net/public/")
   maven("https://repo.codemc.io/repository/maven-releases/")
